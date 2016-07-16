b25d25eaf43109fc6cb7047a6095eaf320a3bcf7
MD Asif Choudhury <bdl365@yahoo.com>
2016-07-16 19:39:06 -0400

    node_modules removed

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -9,29 +9,33 @@ gulp.task('scripts', function(){
     gulp.src('js_src/*.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('../build/js/'));
+        .pipe(gulp.dest('../build/js/'))
+        .pipe(gulp.dest('../js/'));
 
     gulp.src('views/js_src/*.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('../build/views/js/'))
-        .pipe(gulp.dest('../dist/views/js/'));
+        .pipe(gulp.dest('../dist/views/js/'))
+        .pipe(gulp.dest('../views/js/'));
 });
 
 gulp.task('clean-css', function() {
     gulp.src('css_src/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('../build/css/'));
+        .pipe(gulp.dest('../build/css/'))
+        .pipe(gulp.dest('../css/'));
 
     gulp.src('views/css_src/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('../build/views/css/'));
+        .pipe(gulp.dest('../build/views/css/'))
+        .pipe(gulp.dest('../views/css/'));
 });
 
 gulp.task('inlineCss', function() {
-    gulp.src('./*.html')
+    gulp.src('./index.html')
         .pipe(inlineCss({
             applyStyleTags: true,
             applyLinkTags: true,
@@ -40,26 +44,45 @@ gulp.task('inlineCss', function() {
         }))
         .pipe(gulp.dest('../build/'));
 
-    gulp.src('views/*.html')
+    gulp.src('./project-2048.html')
         .pipe(inlineCss({
             applyStyleTags: true,
             applyLinkTags: true,
             removeStyleTags: true,
             removeLinkTags: true
         }))
-        .pipe(gulp.dest('../build/views/'));
+        .pipe(gulp.dest('../build/'));
+
+    gulp.src('./project-mobile.html')
+        .pipe(inlineCss({
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest('../build/'));
+
+    gulp.src('./project-webperf.html')
+        .pipe(inlineCss({
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest('../build/'));
 });
 
 gulp.task('html-min', function() {
     gulp.src('../build/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('../dist/'))
+        .pipe(gulp.dest('../'))
 
-    gulp.src('../build/views/*.html')
+
+    gulp.src('views/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('../dist/views/'))
+        .pipe(gulp.dest('../views/'))
 });
 
 gulp.task('default', ['scripts', 'clean-css', 'inlineCss', 'html-min']);
